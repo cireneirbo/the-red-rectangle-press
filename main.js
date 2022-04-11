44229a41274b152c178fb988ec7484253957413e
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2022-04-11 17:11:51 -0400

    Finished main.js

--- main.js
+++ main.js
@@ -3,21 +3,51 @@ const YoutubeMp3Downloader = require('youtube-mp3-downloader');
 const { Deepgram } = require('@deepgram/sdk');
 const ffmpeg = require('ffmpeg-static');
 const sgMail = require('@sendgrid/mail');
+const input = require('readline-sync');
 require('dotenv').config();
 
 //* Initialize Clients *//
-const deepgram = new Deepgram(process.env.DEEPGRAM_API_KEY)
+const deepgram = new Deepgram(process.env.DEEPGRAM_API_KEY);
 const YD = new YoutubeMp3Downloader({
   ffmpegPath: ffmpeg,
   outputPath: './',
   youtubeVideoQuality: 'lowestaudio',
   queueParallelism: 2,
   progressTimeout: 5000,
-})
+});
 let audioToTextTranscript = "";
 
+//* Get input and instruct the user *//
+console.log(`
+▄▄▄▄▄▄   ▄▄▄▄▄▄▄ ▄▄▄▄▄▄     ▄▄▄▄▄▄   ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄ ▄▄    ▄ ▄▄▄▄▄▄▄ ▄▄▄     ▄▄▄▄▄▄▄    ▄▄▄▄▄▄▄ ▄▄▄▄▄▄   ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ 
+█   ▄  █ █       █      █   █   ▄  █ █       █       █       █      █  █  █ █       █   █   █       █  █       █   ▄  █ █       █       █       █
+█  █ █ █ █    ▄▄▄█  ▄    █  █  █ █ █ █    ▄▄▄█       █▄     ▄█  ▄   █   █▄█ █   ▄▄▄▄█   █   █    ▄▄▄█  █    ▄  █  █ █ █ █    ▄▄▄█  ▄▄▄▄▄█  ▄▄▄▄▄█
+█   █▄▄█▄█   █▄▄▄█ █ █   █  █   █▄▄█▄█   █▄▄▄█     ▄▄█ █   █ █ █▄█  █       █  █  ▄▄█   █   █   █▄▄▄   █   █▄█ █   █▄▄█▄█   █▄▄▄█ █▄▄▄▄▄█ █▄▄▄▄▄ 
+█    ▄▄  █    ▄▄▄█ █▄█   █  █    ▄▄  █    ▄▄▄█    █    █   █ █      █  ▄    █  █ █  █   █▄▄▄█    ▄▄▄█  █    ▄▄▄█    ▄▄  █    ▄▄▄█▄▄▄▄▄  █▄▄▄▄▄  █
+█   █  █ █   █▄▄▄█       █  █   █  █ █   █▄▄▄█    █▄▄  █   █ █  ▄   █ █ █   █  █▄▄█ █       █   █▄▄▄   █   █   █   █  █ █   █▄▄▄ ▄▄▄▄▄█ █▄▄▄▄▄█ █
+█▄▄▄█  █▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄█   █▄▄▄█  █▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█ █▄▄▄█ █▄█ █▄▄█▄█  █▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█  █▄▄▄█   █▄▄▄█  █▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█
+
+>>> Greetings! Welcome to the Red Rectangle Press!
+>>> This is a program which can email you a transcript of a YouTube video.
+>>> It is simple to use, and only requires a few user inputs.
+>>> Let's begin!
+
+>>> Navigate to YouTube to find the video you'd like to extract an audio transcript from.
+>>> An example video is this: 
+https://www.youtube.com/watch?v=0L5TQJbH1gg
+
+>>> Now only copy the video ID, which is the query string that follows the '?v='.
+>>> Our example's is:
+0L5TQJbH1gg
+
+>>> You will be using this video ID string for the first input.
+
+`);
+
+let videoLink = input.question(">>> Paste your video ID here: ");
+
 //* Download Audio From YouTube *//
-YD.download('OVuhW0YhA3Q') // create an input asking for a unique video id
+YD.download(videoLink) // create an input asking for a unique video id
 
 // While downloading...
 YD.on('progress', (data) => {
@@ -47,30 +77,45 @@ YD.on('finished', async (err, video) => {
   const result = await deepgram.transcription
     .preRecorded(file, options)
     .catch((e) => console.log(e));
-  //console.log(result.toWebVTT())
 
   audioToTextTranscript = result.results.channels[0].alternatives[0].transcript;
-  console.log(audioToTextTranscript);
-  // Save the File
-  // const transcript = result.results.channels[0].alternatives[0].transcript;
-  // fs.writeFileSync(
-  //   `${videoFileName}.txt`,
-  //   transcript,
-  //   () => `Wrote ${videoFileName}.txt`
-  // )
 
   // Delete the mp3
   fs.unlinkSync(videoFileName);
 
   //* Sendgrid Email Process *//
+  console.log(`\n>>> Now we need to find out where this transcript is going.\n`);
+  let emailToInput = input.question(">>> Paste the email address that you are sending the transcript to:  ");
 
   // Declare parameters for email
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  const emailTo = process.env.SENDGRID_EMAIL_RECEIVER;
+  const emailTo = emailToInput;
   const emailFrom = process.env.SENDGRID_EMAIL_SENDER;
-  const emailSubject = "SendGrid Test Email";
-  const emailText = audioToTextTranscript;
-  const emailHTML = `<p>${emailText}</p>`;
+  const emailSubject = "transcript of: " + videoFileName + " || The Red Rectangle Press";
+  const emailText = `
+  This is a transcript created using DeepGram, a voice-to-text AI.\n
+  
+  The original video can be viewed here:\n
+  https://www.youtube.com/watch?v=${videoLink}\n\n
+
+  ${audioToTextTranscript}\n\n
+  
+
+  ---------------\n
+  Sent via the Red Rectangle Press (https://github.com/cireneirbo/the-red-rectangle-press)
+  `;
+  const emailHTML = `<div>
+  <h3>This is a transcript created using DeepGram, a voice-to-text AI.</h3>
+  
+  <h4>The original video can be viewed here:</h4>
+  <a href="https://www.youtube.com/watch?v=${videoLink}">https://www.youtube.com/watch?v=${videoLink}</a>
+  <br>
+  <p>${audioToTextTranscript}</p>
+  
+  <br>
+  <h3>---------------</h3>
+  <h4>Sent via the <a href="https://github.com/cireneirbo/the-red-rectangle-press">Red Rectangle Press</a></h4>
+  </div>`;
 
   // Create the email
   const msg = {
